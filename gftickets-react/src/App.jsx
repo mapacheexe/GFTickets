ca4ff72fc1383eed459ba3b368b27d4f081b02fc
fran <fraarrmat11@gmail.com>
2026-07-14 12:43:56 +0200

    fix: fetch quitado del app.jsx

--- gftickets-react/src/App.jsx
+++ gftickets-react/src/App.jsx
@@ -1,35 +1,9 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import { EventsComponent } from './components/EventsComponent.jsx';
-import {findAllEvents} from './services/EventsService.js';
+
 function App() {
-  const [events, setEvents] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const cargarEventos = async () => {
-      try {
-        setLoading(true);
-        const data = await findAllEvents();
-        setEvents(data);
-      } catch (error) {
-        console.log("error al traer los eventos del servicio: ", error);
-      } finally{
-        setLoading(false);
-      }
-    }
-
-    cargarEventos();
-  }, []);
-
   return (
-    <>
-      {loading ? (
-        <div>Cargando eventos...</div>
-      ) : (
-      <EventsComponent eventos={events} />
-      )}
-    </>
+    <EventsComponent />
   )
 }
 

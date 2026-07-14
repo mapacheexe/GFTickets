5d7cb08af15684baeb48476cd50d3d273c1163da
fran <fraarrmat11@gmail.com>
2026-07-14 16:22:44 +0200

    fix: borrar import de use navigate para que pase la condenada pipeline

--- gftickets-react/src/components/EventDetailsComponent.jsx
+++ gftickets-react/src/components/EventDetailsComponent.jsx
@@ -1,4 +1,4 @@
-import {useNavigate, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import { findEventById } from "../services/EventsService";
 import { useEffect, useState } from "react";
 export function EventDetailsComponent (){

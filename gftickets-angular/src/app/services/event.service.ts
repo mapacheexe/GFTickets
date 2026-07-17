452a4bf052fdb32090d2d7653d62ca32a4f19fa4
Julián Adell <doje@gft.com>
2026-07-17 14:06:08 +0200

    chore: centralize Angular API configuration

--- gftickets-angular/src/app/services/event.service.ts
+++ gftickets-angular/src/app/services/event.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { map, Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment';
 import { Evento } from '../models/evento.model';
 import { HoraEvento } from '../models/hora-evento.model';
 
@@ -12,7 +13,7 @@ interface EventoApi extends Omit<Evento, 'horaEvento'> {
 @Injectable({ providedIn: 'root' })
 export class EventService {
   private readonly http = inject(HttpClient);
-  private readonly apiUrl = 'http://teacherbanking.us-east-1.elasticbeanstalk.com/eventos';
+  private readonly apiUrl = `${environment.apiBaseUrl}/eventos`;
 
   findAllEvents(): Observable<Evento[]> {
     return this.http.get<EventoApi[]>(this.apiUrl).pipe(
---
+++ gftickets-angular/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://teacherbanking.us-east-1.elasticbeanstalk.com',
+} as const;
